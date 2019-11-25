935ec6592c41fa688d3f5743a637132a594b78d0
Chedro Cardenas <chegiorca@hotmail.com>
2019-11-24 18:18:16 -0700

    reset to 0

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { interval } from 'rxjs';
+import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-game-control',
   templateUrl: './game-control.component.html',
   styleUrls: ['./game-control.component.css']
+
 })
 export class GameControlComponent implements OnInit {
-
+  //variable counter that will count from 0 or continue
+  counter:number = 0;
+  
+  
+  
+  
+  //variable timer is ID that allows to use 
+  //the clearInterval() method to cancel the timer
+  timer:any; 
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  onStart(){
+    this.timer = setInterval(()=>{
+      this.counter++
+      console.log(this.counter);
+    },1000)
+  }
+
+  onStop(){
+    clearInterval(this.timer);
+    console.log(this.counter);
   }
 
 }
